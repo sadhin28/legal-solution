33a0f189fd8929c3c1dec12a26c3ffc454217228
sadhin28 <taosifsadhin113@gmail.com>
2025-03-23 22:24:39 +0600

    bacround img add

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/image')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
